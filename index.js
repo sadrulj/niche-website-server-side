3538d4fd825d832faf6945f73a43f756730de662
sadrulj <jamil.sadrul@gmail.com>
2021-11-23 19:02:59 +0600

    manage Reviews

--- index.js
+++ index.js
@@ -101,6 +101,13 @@ async function run() {
       res.json(result);
     });
 
+    app.delete("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewsCollection.deleteOne(query);
+      res.json(result);
+    });
+
     app.get("/users/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
