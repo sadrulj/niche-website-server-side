ec00a2dd061d67914448862fa7bd591d0732f5d9
sadrulj <jamil.sadrul@gmail.com>
2022-01-12 14:25:38 +0600

    server

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ediyn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ediyn.mongodb.net/makemyglassesDB?retryWrites=true&w=majority`;
 // console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
